713c4993dc140fd45482ef7dfa3ec3024ab11a83
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-06-22 14:33:23 +0900

    06/22 스테이지 구현, 게임 클리어 구현 중

--- GamePro/Main.cpp
+++ GamePro/Main.cpp
@@ -23,7 +23,7 @@ int main() {
 	_getch();
 	while (true) {
 		system("cls");
-		if (gameClear == false && CheckGameEnd() == true) {
+		if (!gameClear && CheckGameEnd()) {
 			cout << "축하합니다 모든 스테이지를 클리어하셨습니다." << endl;
 			cout << "플레이 해주셔서 감사합니다." << endl;
 			cout << "키를 누르면 스테이지 클리어 화면으로 돌아갑니다." << endl;
@@ -32,7 +32,7 @@ int main() {
 			gameClear = true;
 		}
 		cout << stageClear[0] << stageClear[1] << stageClear[2] << endl;
-		cout << gameClear << endl << endl;
+		cout << gameClear << CheckGameEnd() << endl << endl;
 		cout << "STAGE 1" << endl << "STAGE 2" << endl << "STAGE 3" << endl;
 		cout << "번호를 입력하여 스테이지를 선택하세요." << endl << "(0 - 종료, 9 - 설명) : ";
 		cin >> stage;
@@ -61,6 +61,7 @@ int main() {
 			continue;
 		}
 		else if (stage == 0) { break; }
+		else if (stage >= 10) { stageClear[stage - 10] = true; continue; }
 		else if (stage > 3 || stage < 0) { continue; }
 		else if (stage != 1 && stageClear[stage - 2] == false) {
 			cout << "이전 스테이지를 먼저 클리어해주세요." << endl;
